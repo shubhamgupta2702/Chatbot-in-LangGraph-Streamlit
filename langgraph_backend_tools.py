e71338b1b5072edb5588299036212cbffcb75d30
Shubham <shubhamgupta43567@gmail.com>
2026-04-02 01:04:43 +0530

    Chatbot with Tools

--- langgraph_backend_tools.py
+++ langgraph_backend_tools.py
@@ -57,7 +57,7 @@ def get_stock_price(symbol: str) -> dict:
     Fetch latest stock price for a given symbol (e.g. 'AAPL', 'TSLA') 
     using Alpha Vantage with API key in the URL.
     """
-    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=C9PE94QUEW9VWGFM"
+    url = f"AlphaVantageAPI"
     r = requests.get(url)
     return r.json()
 
